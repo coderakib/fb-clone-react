91e41756783f4477e6f0a72b1342faec06c3a039
Akib Hussain <coder.akib0903@gmail.com>
2021-10-29 08:30:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    //Bem naming convention
+    <div className="app">
+      <h1>Hi CoderAkib!</h1>
+    </div>
+  );
+}
+
+export default App;
